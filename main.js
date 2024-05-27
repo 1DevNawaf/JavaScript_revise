4e68363fa6a44c533776aedff2ccb72848344770
NawafAlGhodyah <nalghodyah.t@barraq.com.sa>
2024-05-27 11:31:13 +0300

    readlie

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log('Hello world');
+const readline = require('readline');
+
+// Create an interface for reading data from the terminal
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+// Prompt the user for input
+rl.question('Please enter your name: ', (name) => {
+    // Store the user's input in a variable
+    const userName = name;
+
+    // Print the variable
+    console.log(`Hello, ${userName}!`);
+
+    // Close the readline interface
+    rl.close();
+});
+
+
+
