bbdc3ee7c4f164e231a381bac10e8f7c5691bcf0
NawafAlGhodyah <nalghodyah.t@barraq.com.sa>
2024-05-27 12:11:45 +0300

    extended arrow function

--- main.js
+++ main.js
@@ -6,18 +6,34 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-// Prompt the user for input
-rl.question('Please enter your name: ', (name) => {
-    // Store the user's input in a variable
-    greet(name)
+function main(){
+    askForName()
+}
+
+function askForName(){
+    rl.question('Please enter your name: ', (name) => {
+        // Store the user's input in a variable
+        greetV2(name)
 
 
-    // Close the readline interface
-    rl.close();
-});
-
+        // Close the readline interface
+        rl.close();
+    });
+}
 
 const greet = (name) => console.log("your name is " + name);
+
+const greetV2 = (name) => {
+    if (name.toLowerCase() === 'nawaf'){
+        console.log(`Hail king ${name}`)
+    }else {
+        console.log("your name is " + name)
+    }
+}
+
+
+
+main()
 
 
 
